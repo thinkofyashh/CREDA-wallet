dc3a6d42ead174fdbe9c0310180ad71beea8b7ca
yashrawat <yashrawat682@gmail.com>
2025-07-20 07:20:09 +0530

    created new page for peer to peer transfer

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -9,6 +9,7 @@ export default function Layout({children}:{children:React.ReactNode;}):JSX.Eleme
             <SideBarItem href="/dashboard" title="Home" icon={<HomeIcon></HomeIcon>}></SideBarItem>
             <SideBarItem href="/transfer" title="Transfer" icon={<TransferIcon></TransferIcon>}></SideBarItem>
             <SideBarItem href="/transaction" title="Transaction" icon={<TransactionIcon></TransactionIcon>}></SideBarItem>
+            <SideBarItem href="/p2ptransfer" title="Send Money" icon={<Peer2PeerIcon></Peer2PeerIcon>}></SideBarItem>
         </div>
         <div className="flex-1 p-8 pt-28">
 
@@ -77,6 +78,22 @@ function HomeIcon() {
       </svg>
     );
   }
-  
+  function Peer2PeerIcon(){
+    return (
+        <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        fill="none" 
+        viewBox="0 0 24 24" 
+        stroke-width="1.5" 
+        stroke="currentColor" 
+        className="size-6">
+  <path 
+  strokeLinecap="round" 
+  strokeLinejoin="round" 
+  d="M15 8.25H9m6 3H9m3 6-3-3h1.5a3 3 0 1 0 0-6M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+</svg>
+
+    )
+  }
 
 
---
+++ apps/web/app/(dashboard)/p2ptransfer/page.tsx
@@ -0,0 +1,10 @@
+
+
+
+
+export default function P2ptransfer(){
+    return <div>
+        <h1>Send Money to your Friends </h1>
+    </div>
+
+}
