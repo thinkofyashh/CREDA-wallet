115aa3695485afb673299dc1eeb74f24a9efe228
yashrawat <yashrawat682@gmail.com>
2025-07-23 22:06:23 +0530

    fixed route.ts error

--- apps/web/app/api/auth/[...nextauth]/route.ts
+++ apps/web/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,6 @@ import NextAuth from "next-auth";
 import { authConfig } from "../../../../lib/auth";
 
 
-export const handler=NextAuth(authConfig);
+ const handler=NextAuth(authConfig);
 
 export {handler as GET,handler as POST}
